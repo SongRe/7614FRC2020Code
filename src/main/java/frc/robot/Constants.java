7434336ce8845954c966999c83f93d7026998de0
edieb <songks712@gmail.com>
2020-01-30 14:47:51 -0500

    Need to get WPI_TalonSRX working

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final int leftMasterPort = 1; //left side of the drive train master motor
+    public static final int leftSlavePort = 2;  //left side of drive train slave motor (there are two motors per side of the drivetrain)
+
+    public static final int rightMasterPort = 3;  //same thing but for right
+    public static final int rightSlavePort = 4;
+
+    public static final int joystickPort = 0;
+
+    //Multiple Modules, define both the port number and the module. For example:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+
+    //PWM cable
+
+    //DIO (digital input output)
+
+    //CAN (bus) Idk what this is
+
+    //USB ports
+
+    //Other constants
+
+    //Constants
+    //example: 
+    // public static int driveSpeed = 100;
+
+
+
+
+
+
+
+
+    
+}
